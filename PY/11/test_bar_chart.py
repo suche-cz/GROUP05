41bd49bebacf9e5cd37c854c1bab8b2a429d8121
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2024-09-06 07:18:55 +0200

    úkoly + zdroje

--- PY/11/test_bar_chart.py
+++ PY/11/test_bar_chart.py
@@ -1,16 +1,12 @@
 import matplotlib.pyplot as plt
 
 # Data
-data = {
-    'Apples': 10,
-    'Bananas': 15,
-    'Cherries': 7,
-    'Dates': 5
-}
+fruits = ['Apples', 'Bananas', 'Cherries', 'Dates']
+data = [10, 15, 7, 5]
 
 # Bar plot
 plt.figure(figsize=(8, 5))
-plt.bar(data.keys(), data.values(), color='purple')
+plt.bar(fruits, data, color='purple')
 plt.title("Fruit Sales")
 plt.xlabel("Fruit")
 plt.ylabel("Quantity Sold")
