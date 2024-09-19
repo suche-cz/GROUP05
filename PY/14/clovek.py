a898099c1dc0eda390c83085aafefd8131a37f95
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2024-09-19 06:46:18 +0200

    PY15
    pracovali jsme hlavně v PY14/clovek.py

--- PY/14/clovek.py
+++ PY/14/clovek.py
@@ -9,8 +9,17 @@ atributy:
 - popis
 
 vytvořte 2 instance z třídy Clovek
+
+def metoda(self) = instanční metoda = pracuje s danou instancí
+
+@classmethod
+def metada(cls) = třídní metoda (class method) = metoda, která pracuje s třídou
+
 """
 import datetime as dt
+
+with open('PY/14/template.html', encoding='utf-8') as file:
+    TEMPLATE = file.read()
 
 class Clovek:
     def __init__(self, _id, jmeno, datum_narozeni, popis):
@@ -28,6 +37,10 @@ class Clovek:
         # string reprezentace pro vytvoření kopie
         # také nám to ukáže objekt v datové struktuře
         return str(self)
+
+    @classmethod
+    def spolecni_pratele(cls, clovek1: 'Clovek', clovek2: 'Clovek'):
+        return clovek1.pratele & clovek2.pratele
     
     @property
     def vek(self):
@@ -49,26 +62,49 @@ class Clovek:
     def odeber_pritele(self, clovek: 'Clovek'):
         if clovek in self.pratele:
             self.pratele.remove(clovek)
+    
+    def pridej_vice_pratel(self, list_pratel: list):
+        for clovek in list_pratel:
+            self.pridej_pritele(clovek)
+
+    def vytvor_html(self):
+        html = TEMPLATE
+
+        d = self.datum_narozeni
+        datum_narozeni = f'{d.day}. {d.month}. {d.year}'
+
+        replace_values = (
+            ('{id}', str(self.id)),
+            ('{jmeno}', self.jmeno),
+            ('{vek}', str(self.vek)),
+            ('{datum_narozeni}', datum_narozeni),
+            ('{popis}', self.popis),
+            ('{pratele}', self.get_html_pratel()),
+        )
+
+        for key, value in replace_values:
+            html = html.replace(key, value)
+        
+        html_path = f'PY/14/html/{self.id}.html'
+        with open(html_path, mode='w', encoding='utf-8') as file:
+            file.write(html)
+    
+    def get_html_pratel(self):
+        html = ''
+        for item in self.pratele:
+            html += f'<li>{item.jmeno}</li>'
+
+        return html
 
 
 jana = Clovek(1, 'Jana', dt.date(2000, 4, 12), 'Hello I am Jana')
 petr = Clovek(2, 'Petr', dt.date(1997, 10, 20), 'Hello I am Petr')
 suche = Clovek(3, 'Suche', dt.date(1900, 12, 20), "I'll be Back!")
 
-print(jana.to_dict())
-print(petr.to_dict())
+jana.pridej_vice_pratel([petr, suche, jana])
+petr.pridej_pritele(suche)
+print(jana.pratele)
+print(Clovek.spolecni_pratele(jana, petr))
 
-suche.pridej_pritele(jana)
-suche.pridej_pritele(petr)
-# suche.pridej_pritele(jana, petr) #toto není připrave
-
-jana.pridej_pritele(petr)
-# jana.pridej_pritele(jana)
-petr.pridej_pritele(jana)
-
-jana.odeber_pritele(petr)
-jana.pridej_pritele(suche)
-
-print('Jana:', jana.pratele)
-print('Petr:', petr.pratele)
-print('Suche:', suche.pratele)
+jana.vytvor_html()
+petr.vytvor_html()
