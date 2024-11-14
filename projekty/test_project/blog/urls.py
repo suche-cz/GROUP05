6bd053163ab83a6f84bda3e65af331544ed22834
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2024-11-14 08:26:33 +0100

    PY27

--- projekty/test_project/blog/urls.py
+++ projekty/test_project/blog/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, about, cislo, username
 
 urlpatterns = [
-    path('', index),
+    path('', index), # /blog/
+    path('about/', about), # /blog/about/
+    # /blog/cislo/1000/
+    # /blog/cislo/100/
+    path('cislo/<int:number>/', cislo),
+    path('username/<str:name>/', username),
+
+    # /username/suche/
+    # /username/jan/
+    # /username/petr/
 ]
--- projekty/test_project/blog/views.py
+++ projekty/test_project/blog/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def index(request):
     print(request.POST)
     return render(request, 'blog/index.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def cislo(request, number):
+    context = {'number': number}
+    return render(request, 'blog/cislo.html', context)
+
+def username(request, name):
+    context = {'name': name} # pod názvem 'name' to bude k dispozici v template
+    return render(request, 'blog/username.html', context)
